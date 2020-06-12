d848d67b03e31e0e67fe5cdb566358af2df2ade2
Panda88CO <65139210+Panda88CO@users.noreply.github.com>
2020-06-11 23:12:33 -0700

    Update temp-poly.py
    fixws 24H table

--- temp-poly.py
+++ temp-poly.py
@@ -2,7 +2,6 @@
 
 import polyinterface
 import sys
-import json
 import RPi.GPIO as GPIO
 import os
 import glob
@@ -27,9 +26,10 @@ class Controller(polyinterface.Controller):
         try:
             os.system('modprobe w1-gpio')
             os.system('modprobe w1-therm')
+            self.setDriver('ST', 1)
         except:
-            LOGGER.info('modprobe OS calls not successful')
-
+            LOGGER.debug('modprobe OS calls not successful')
+            self.setDriver('ST', 0)
 
     def start(self):
         LOGGER.info('start - Temp Sensor controller')
@@ -37,30 +37,32 @@ class Controller(polyinterface.Controller):
             self.mySensors = W1ThermSensor()
             self.nbrSensors = len(self.mySensors.get_available_sensors())
             LOGGER.info( str(self.nbrSensors) + ' Sensors detected')
-            self.discover()  
+            self.discover()
+             self.setDriver('ST', 1)
         except:
             LOGGER.info('ERROR initializing w1thermSensors ')
-            #self.stop()
+            self.setDriver('ST', 0)
+            self.stop()
         self.updateInfo()
+        self.reportDrivers()
 
     def stop(self):
-        #W1ThermSensor.get_available_sensors()
-        LOGGER.info('stop - Cleaning up Temp Sensors')
+        LOGGER.debug('stop - Cleaning up Temp Sensors')
 
 
     def shortPoll(self):
-        LOGGER.info('shortPoll')
+        LOGGER.debug('shortPoll')
         for node in self.nodes:
             self.nodes[node].updateInfo()
             
     def longPoll(self):
-        LOGGER.info('longPoll')
+        LOGGER.debug('longPoll')
         for node in self.nodes:
             self.nodes[node].updateInfo()
             self.nodes[node].update24Hqueue()
 
     def update24Hqueue (self):
-         LOGGER.info('Update24H queue')
+         LOGGER.debug('Update24H queue')
          pass
 
     def updateInfo(self):
@@ -71,7 +73,7 @@ class Controller(polyinterface.Controller):
         LOGGER.info('querry Info')
         for node in self.nodes:
             self.nodes[node].updateInfo()
-            self.nodes[node].reportDrivers()
+            self.nodes[node].update24Hqueue()
 
     def discover(self, command=None):
         LOGGER.info('discover')
@@ -79,32 +81,23 @@ class Controller(polyinterface.Controller):
         for mySensor in self.mySensors.get_available_sensors():
             count = count+1
             currentSensor = mySensor.id.lower()
-            LOGGER.debug(currentSensor)
+            LOGGER.info(currentSensor + 'Sensor Serial Number Detected - use custom params to rename')
             address = 'rpitemp'+str(count)
             if currentSensor in self.polyConfig['customParams']:
-               LOGGER.info('A customParams name for sensor detected')
+               LOGGER.debug('A customParams name for sensor detected')
                name = self.polyConfig['customParams'][currentSensor]
             else:
                LOGGER.debug('Default Naming')
                name = 'Sensor'+str(count)
             
-            LOGGER.info( address + ' '+ name + ' ' + currentSensor)
+            LOGGER.debug( address + ' '+ name + ' ' + currentSensor)
             if not address in self.nodes:
                self.addNode(TEMPsensor(self, self.address, address, name, currentSensor))
 
     def check_params(self, command=None):
         # Looking for custom defined names - allowing sensor detection order to change and not affect ISY
-        LOGGER.info('Getting Sensor Names from custom Params' )
-        #i = 0 
-        #for mySensor in W1ThermSensor.get_available_sensors():     
-        #    i = i+1
-        #    if mySensor.id in self.polyConfig['customParams']:
-        #       LOGGER.info('A customParams for name for sensor detected')
-        #       self.name[i] = self.polyConfig['customParams'][mySensor.id]
-        #    else:
-        #       self.name[i] = 'TempSensor'+str(i) 
-        #       LOGGER.info('Default Sensor Name added' + self.name[i])
-
+        LOGGER.info('Check Params' )
+       
     id = 'RPITEMP'
     commands = {'DISCOVER': discover}
     drivers = [{'driver': 'ST', 'value': 1, 'uom': 2}]
@@ -124,46 +117,48 @@ class TEMPsensor(polyinterface.Node):
         self.tempC = self.sensor.get_temperature(W1ThermSensor.DEGREES_C)
         self.tempMinC24H = self.tempC
         self.tempMaxC24H = self.tempC
-        self.tempShortPollMin = self.tempC
-        self.tempShortPollMax = self.tempC
+        self.tempMinC24HUpdated = False
+        self.tempMaxC24HUpdated = False
         self.currentTime = datetime.datetime.now()
         self.updateInfo()
-        LOGGER.info(str(self.tempC) + ' TempSensor Reading')
-        return True
+        LOGGER.debug(str(self.tempC) + ' TempSensor Reading')
+        #return True
 
     def stop(self):
         LOGGER.info('STOP - Cleaning up Temp Sensors')
         #self.sensor = None
 
 
-
     def update24Hqueue (self):
         timeDiff = self.currentTime - self.startTime
-        if timeDiff.days <= 1:
-            self.queue24H.append(self.tempC)
-            if self.tempC < self.tempMinC24H:
-                self.tempMinC24H = self.tempC 
-                LOGGER.debug('24H temp table updated Min')
-            elif self.tempC > self.tempMaxC24H:
-                self.tempMaxC24H = self.tempC 
-                LOGGER.debug('24H temp table updated Max')
+        if self.tempMinC24HUpdated:
+            self.queue24H.append(self.tempMinC24H)
+            LOGGER.debug('24H temp table updated Min')
+        elif self.tempMaxC24HUpdated:
+            self.queue24H.append(self.tempMaxC24H) 
+            LOGGER.debug('24H temp table updated Max')
         else:
-            self.queue24H.put(self.tempC)
+            self.queue24H.append(self.tempC)
+        if timeDiff.days >= 1:         
             temp = self.queue24H.pop()
-            if ((temp == self.tempMaxC24H) or (temp == self.tempMaxC24H)):
+            if ((temp == self.tempMinC24H) or (temp == self.tempMaxC24H)):
                 self.tempMaxC24H = max(self.queue24H)
                 self.tempMinC24H = min(self.queue24H)
-                LOGGER.debug('24H temp table updated')
-        pass
+        LOGGER.debug('24H temp table updated')
+        self.tempMinC24HUpdated = False
+        self.tempMaxC24HUpdated = False
+        #pass
 
     def updateInfo(self):
         LOGGER.info('TempSensor updateInfo')
         self.sensor = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20, self.sensorID )
         self.tempC = self.sensor.get_temperature(W1ThermSensor.DEGREES_C)
-        if self.tempC < self.tempShortPollMin:
-            self.tempShortPollMin = self.tempC 
-        elif self.tempC > self.tempShortPollMax:
-            self.tempShortPollMax = self.tempC         
+        if self.tempC < self.tempMinC24H:
+            self.tempMinC24H = self.tempC
+            self.tempMin24HUpdated = True
+        elif self.tempC > self.tempMaxC24H:
+            self.tempMaxC24H = self.tempC
+            self.tempMax24HUpdated = True
         self.currentTime = datetime.datetime.now()
         self.setDriver('GV0', round(float(self.tempC),1))
         self.setDriver('GV1', round(float(self.tempMinC24H),1))
@@ -179,7 +174,7 @@ class TEMPsensor(polyinterface.Node):
         self.setDriver('ST', 1)
         self.reportDrivers()
 
-        return True                                                    
+        #return True                                                    
         
     
     def query(self, command=None):
@@ -209,7 +204,7 @@ class TEMPsensor(polyinterface.Node):
 if __name__ == "__main__":
 #    signal.signal(signal.SIGTERM, signal_term_handler)
     try:
-        LOGGER.info('Starting Server COE')
+        LOGGER.info('Starting Temperaure Server')
         polyglot = polyinterface.Interface('Temp_Sensors')
         
         polyglot.start()
